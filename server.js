5fe48d837e364b8793e456834eb66b3254c38779
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-14 12:57:37 -0800

    changed erroneously named controllers folder and file to routes

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./routes/routes");
 
 app.use(routes);
 app.use(express.static('public'));
